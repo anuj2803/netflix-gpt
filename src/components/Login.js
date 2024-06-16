48020e702f639e39812d856c1b6ce0ba677a7b0e
anuj2803 <khedekaranuj001@gmail.com>
2024-06-16 17:52:51 +0530

    Fixed dynamic programming solution for max damage calculation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,23 @@
+import { useState,useRef } from "react";
 import Header from "./Header";
-
+import { checkValidData } from "../util/validate";
+// import userEvent from "@testing-library/user-event";
 const Login = () => {
+  const [isSignInForm,setIsSignInForm]=useState(true);
+//to store msg we use usestate
+  const[errorMessage,setErrorMessage]=useState(null);
+
+  // const name=useRef(null);
+  const email=useRef(null);
+  const password=useRef(null);
+
+  const handleButtonClick=()=>{
+     const message=checkValidData(email.current.value,password.current.value);
+      setErrorMessage(message);
+  };
+  const toggleSignInForm=()=>{
+         setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
         <Header/>
@@ -12,18 +29,41 @@ const Login = () => {
         alt="logo"
         />
     </div>
-    <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white">
-        <h1 className="font-bold text-3xl py-4">Sign  In</h1>
+    <form onSubmit={(e)=>e.preventDefault()}className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm?"Sign In":"Sign Up"}
+
+        </h1>
+        {
+          !isSignInForm&&(
+            <input
+            // ref={name}
+            type="text" 
+            placeholder="Full Name"
+             className="p-4 my-4 w-full bg-gray-700"/>
+          )
+        }
         <input
+        ref={email}
          type="text" 
          placeholder="Email Address"
-          className="p-2 m-2 w-full"/>
+          className="p-4 my-4 w-full bg-gray-700"/>
         <input
+        ref={password}
          type="password"
           placeholder="password"
-           className="p-2 m-2 w-full"/>
-        <button className="p-4 m-4 bg-red-700 w-full">Sign In</button>
+           className="p-4 my-4 w-full bg-gray-700"/>
+           <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button 
+        className="p-4 my-6 bg-red-700 w-full rounded-lg   "onClick={handleButtonClick}>
+        
+        {!isSignInForm?"Sign Up":"Sign In"}
+        </button>
 
+        {/* on single form sign in and signup facility provided due to toggle action */}
+         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+         {!isSignInForm?"already user,then sign in":"new user,Sign Up"}
+          </p>
     </form>
     </div>
   );
